921c68f1e0b8ea11a355a3bddd748f6ac52c2b62
Michal Mühlpachr <michal.muhlpachr@bigclown.com>
2017-11-02 13:00:41 +0100

    Fix paths in dfu-driver-install paths

--- script/dfu-driver-install.cmd
+++ script/dfu-driver-install.cmd
@@ -1,2 +1,2 @@
-"%BigClown%\dfu\zadic.exe" --vid 0x0483 --pid 0xDF11 --create stm32dfu
-rmdir /S /Q "%BigClown%\dfu\usb_driver"
+"%BigClownToolchain%\dfu\zadic.exe" --vid 0x0483 --pid 0xDF11 --create stm32dfu
+rmdir /S /Q "%BigClownToolchain%\dfu\usb_driver"
